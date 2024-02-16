750cecdc3799bf13952d1f065e3d46e1d54838cf
park-david <davidpark1094@gmail.com>
2024-02-15 20:41:02 -0800

    finished content placeholder

---
+++ 24_content_placeholder/script.js
@@ -0,0 +1,22 @@
+const header = document.getElementById('header')
+const title = document.getElementById('title')
+const excerpt = document.getElementById('excerpt')
+const profile_img = document.getElementById('profile_img')
+const name = document.getElementById('name')
+const date = document.getElementById('date')
+
+const animated_bgs = document.querySelectorAll('.animated-bg')
+const animated_bgs_text = document.querySelectorAll('.animated-bg-text')
+
+setTimeout(getData, 2500)
+
+function getData() {
+    header.innerHTML = '<img src="https://images.unsplash.com/photo-1531297484001-80022131f5a1?q=80&w=1420&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">'
+    title.innerHTML = 'Lorem ipsum dolor sit amet.'
+    excerpt.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, omnis?'
+    profile_img.innerHTML = '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="">'
+    name.innerHTML = 'John Doe'
+    date.innerHTML = 'Oct 08, 2020'
+    animated_bgs.forEach(bg => bg.classList.remove('animated-bg'))
+    animated_bgs_text.forEach(bg => bg.classList.remove('animated-bg-text'))
+}
